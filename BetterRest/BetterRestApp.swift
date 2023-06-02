641fa38df854adab55c36800ecabd82032c5036c
fauzandp <fauzan27dp@gmail.com>
2023-06-02 22:09:37 +0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Fauzan Dwi Prasetyo on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
